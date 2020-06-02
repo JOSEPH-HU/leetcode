d2f3aec41cf6c6a2912e5a162751fe32f92a0b51
huliwei <huliwei@yinxiang.com>
2020-06-02 10:42:20 +0800

    文件改名

---
+++ src/main/java/MovesToMakeZigzag.java
@@ -0,0 +1,22 @@
+public class MovesToMakeZigzag{
+	public static void main(String[] args) {
+		
+	}
+
+	public static int solution(int[] nums){
+		int count_a = 0;//偶索引
+		int count_b = 0;//奇索引
+		int len = nums.length;
+		int pre = 0;
+		int after = 0;
+
+		for(int i=0;i<len-1;i++){
+			if((i)%2==0){
+				//偶数索引	
+
+			}
+			}
+		}
+		
+	}
+}
